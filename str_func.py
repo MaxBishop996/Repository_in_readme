8c22c18fdda2e5bbffdfe43fb5938685721b8da1
Maksim Karebin <karebin.maxim@gmail.com>
2023-08-14 15:16:23 +0300

    add strcap
    and create docstring in def

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def strupper(string: str):
+    """
+    функция делает буквы заглавными
+    :param string: входная строка
+    :return:
+    """
     return string.upper()
+
+
+def strcap(string: str):
+    """
+    функция делает первую букву заглавной
+    :param string: входная строка
+    :return:
+    """
+    return string.capitalize()
